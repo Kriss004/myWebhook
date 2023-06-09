cb8430ab744b4cd8a0397c877c9ddafea04c986d
Gustavo <kkriss254@gmail.com>
2023-06-09 13:57:08 +0300

    r

--- controller.js
+++ controller.js
@@ -50,7 +50,7 @@ let getWebhook = (req, res) => {
     const payload = {
         object: 'page',
         callback_url: 'https://bomatext.herokuapp.com/webhook',
-        fields: ['messages', 'messaging_postbacks', 'messaging_optins'],
+        subscribed_fields: ['messages', 'messaging_postbacks', 'messaging_optins'],
         verify_token: 'myverifytoken'
     };
 
