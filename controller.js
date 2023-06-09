edf75b52b2a447bb806a30d631cd8ff53ab2e04a
Gustavo <kkriss254@gmail.com>
2023-06-09 12:28:05 +0300

    r

--- controller.js
+++ controller.js
@@ -31,7 +31,14 @@ let getWebhook = (req, res) => {
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
 
-    res.sendStatus(200).send(challenge);
+    //res.send(challenge);
+
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        console.log(`WEBHOOK_VERIFIED. Challenge: ${challenge} `);
+        res.send(challenge);
+    } else {
+        res.sendStatus(403);
+    }
 
     /*if (mode && token) {
 
