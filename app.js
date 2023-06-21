2fa9a5813bb2ca5165e880fe0adc5b9663971e58
Gustavo <kkriss254@gmail.com>
2023-06-21 16:45:17 +0300

    crm

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ import express from 'express';
 import controller from './controller.js';
 import bodyParser from 'body-parser';
 import conn from './database.js';
+import cheerio from 'cheerio';
+import querystring from 'querystring';
+import { URL } from 'url';
+
 
 
 const { urlencoded, json } = bodyParser;
@@ -67,9 +71,9 @@ const handleMessage = async (senderPsid, phone_number, receivedMessage) => {
       console.error(error);
     };
     // trying to put data directly into boma crm system... currently incomplete:
-    /*
+
     const session = axios.create({
-      baseURL: 'https://www.bomamabati.co.ke/boma/',
+      baseURL: 'https://www.bomamabati.co.ke/boma',
     });
 
     const loginData = {
@@ -77,31 +81,64 @@ const handleMessage = async (senderPsid, phone_number, receivedMessage) => {
       password: 'chris@boma',
     };
 
-    session.post('/login', loginData).then(() => {
-      // Navigate to the New Lead page
-      return session.get('/crm/createweblead.php');
-    }).then(() => {
-      
-      const leadData = {
-        name: `${username}`,
-        //email: 'john.doe@example.com',
-        //phone: '555-555-5555',
-        message: `${receivedMessage.text}`
-      };
+    session.post('/login', loginData)
+      .then(() => {
+        // Navigate to the New Lead page
+        return session.get('/crm/createweblead.php');
+      }).then(response => {
 
-      return session.post('/crm/createweblead.php', leadData);
-    }).then(() => {
-      // Save the lead
-      return session.post('/save_lead');
-    }).then(response => {
-      console.log('Lead created successfully:', response.data);
-    }).catch(error => {
-      console.error('Error creating lead:', error);
-    });
-    
+        const formHtml = response.data;
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(formHtml, 'text/html');
+
+        const form = doc.querySelector('form');
+
+        const formData = {};
+        const fields = form.querySelectorAll('input,textarea');
+        fields.forEach(field => {
+          formData[field.name] = field.value;
+        });
+
+        formData.fname = username;
+        formData.mobile = phone_number;
+        formData.email = "";
+        formData.ltype = "";
+        formData.mess = receivedMessage.text;
+
+
+        /*
+        const leadData = {
+          name: `${username}`,
+          //email: 'john.doe@example.com',
+          //phone: '555-555-5555',
+          message: `${receivedMessage.text}`
+        };
+        */
+
+        const params = new URLSearchParams(formData).toString();
+
+        const url = new URL('/saveweblead1.php', session.defaults.baseURL);
+        return session.post(url.href, params, {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        });
+
+        //return session.post('/crm/createweblead.php', leadData);
+
+      /*}).then(() => {
+        // Save the lead
+        return session.post('/save_lead');
+        */
+      }).then(response => {
+        console.log('Lead created successfully:', response.data);
+      }).catch(error => {
+        console.error('Error creating lead:', error);
+      });
+
     //////END COMMENTS RIGHT HERE
 
-    */
+
   };
 };
 
